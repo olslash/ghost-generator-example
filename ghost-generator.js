d12cdf14908ba8ddb18fddf69b1c68ddbd3c16e7
Mitch R <olordhux+github@gmail.com>
2015-05-15 21:53:39 -0700

    working concept

---
+++ ghost-generator.js
@@ -0,0 +1,54 @@
+var _ = require('lodash');
+var Promise = require('Bluebird');
+
+
+
+var speak = function* (greetings, script) {
+  yield _.sample(greetings);
+
+  while(true) {
+    var line = _.sample(script);
+
+    if(Array.isArray(line)) {
+      for(let bit of line) {
+        yield bit;
+      }
+    } else {
+      yield line;
+    }
+  }
+}
+
+var awaitUserInput = Promise.coroutine(function* () {
+  yield new Promise(function(resolve) {
+    process.stdin.on('data', function (data) {
+      console.log('user says:', data.toString());
+      process.stdin.removeAllListeners();
+      resolve();
+    });
+  })
+});
+
+
+// main ----------------
+
+var ghostGreetings = [
+  'Hi %s, pleased to be chattin',
+  '%s.... That was the name of an old friend. Hello :)'
+];
+
+var ghostScript = [
+  'I died in a fire',
+  'The world\'s really old',
+  ['first in series', 'second in series', 'third in series']
+];
+
+
+var ghost = speak(ghostGreetings, ghostScript);
+
+function run() {
+  console.log('ghost says: ', ghost.next().value);
+  awaitUserInput().then(run);
+}
+
+run();
